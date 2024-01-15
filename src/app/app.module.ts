9644f76136aa984b7d17bf04645a78631d0e4792
najehsalem <najehsalem2023@gmail.com>
2024-01-15 17:34:40 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { PostItemComponent } from './post-item/post-item.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthentificationComponent,
+    PostItemComponent,
+    PostListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
